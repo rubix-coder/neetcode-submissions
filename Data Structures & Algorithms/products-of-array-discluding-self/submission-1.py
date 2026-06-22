2b176b000345622733820c2993cf9ed700f84928
Jesal Patel <p.jesal.work@gmail.com>
2026-06-22 22:36:52 +0530

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.py
@@ -0,0 +1,13 @@
+def mul(numbers):
+    prod = 1
+    for num in numbers:
+        prod*=num
+    return prod
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        prod_list = []
+        for i in range(len(nums)):
+            prod_list.append(mul(nums[:i]+nums[i+1:]))
+        return prod_list
+        
